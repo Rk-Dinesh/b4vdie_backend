ca61d54296a2f588a9531e2453f59f4d942f376f
Rk-Dinesh <rdkumar881@gmail.com>
2024-04-02 15:58:24 +0530

    cors

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const body_parser = require('body-parser');
 const Router = require('./routers/router');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    "Access-Control-Allow-Origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+}));
 app.use(express.static("img"))
 app.use(body_parser.json());
 app.use('/',Router);
