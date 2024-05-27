70c3987cd0d032706be380492039b547cd527b0b
Rk-Dinesh <rdkumar881@gmail.com>
2024-05-27 13:33:14 +0530

    ip

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = mongoose.createConnection('mongodb://b4vdieAdmin:admin@51.21.55.30:27017/b4vdie_db?directConnection=true').on('open',()=>{
+const connection = mongoose.createConnection('mongodb://b4vdieAdmin:admin@3.26.156.157:27017/b4vdie_db?directConnection=true').on('open',()=>{
     console.log("Mongo DB Connected");
 }).on('error',()=>{
     console.log("Mongo DB Connection Error")
